241bcd936275d934d6490cb56f6899fa2d4b3ad7
Intruder <syzdalcevis88@gmail.com>
2025-03-11 17:59:39 +0400

    remake

---
+++ src/pages/PurchareHistory.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react"
+
+const PurchareHistory = () =>{
+    const [orders, setOrders] = useState([])
+    const [louding, setLoading] = useState(true)
+
+    useEffect(() =>{
+        const fetchOrders = async () => {
+            try{
+                const response = await fetch('http://localhost:3001/orders')
+                if(!response.ok) {
+                    trow new Error('Ошибка призагрузке истории заказов')
+                } finally{
+                    setLoading(false)
+                }
+            }
+            
+        }
+        fetchOrders()
+    }, {})
+}
